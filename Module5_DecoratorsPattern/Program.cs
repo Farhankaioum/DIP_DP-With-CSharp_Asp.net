00164b01e337c24004886b63d19e183f08a26490
Farhan Kaioum <km1472233@gmail.com>
2020-11-05 13:08:03 +0600

    register type using decorator pattern

---
+++ Module5_DecoratorsPattern/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Autofac;
+
+namespace AutofacSamples
+{
+    public interface IReportingService
+    {
+        void Report();
+
+    }
+
+    public class ReportingService : IReportingService
+    {
+        public void Report()
+        {
+            Console.WriteLine("Here is your report");
+        }
+    }
+
+    public class ReportingServiceWithLogging : IReportingService
+    {
+        private IReportingService decorated;
+
+        public ReportingServiceWithLogging(IReportingService decorated)
+        {
+            this.decorated = decorated;
+        }
+
+        public void Report()
+        {
+            Console.WriteLine("Commencing log...");
+            decorated.Report();
+            Console.WriteLine("Ending log...");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ContainerBuilder();
+            b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+            b.RegisterDecorator<IReportingService>(
+              (context, service) => new ReportingServiceWithLogging(service), "reporting"
+            ); // if you want to inherit a interface and also inject this interface, this interface use another class
+                            // you want invoke inject type members then for registration using decorator pattern
+
+            using (var c = b.Build())
+            {
+                var r = c.Resolve<IReportingService>();
+                r.Report();
+            }
+        }
+    }
+}
